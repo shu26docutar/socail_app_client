254986d251675e94d5e19f952e6965e59d34cb44
Shuhei Hattori <62091655+shu26docutar@users.noreply.github.com>
2022-10-27 17:17:17 +0900

    URL追加

--- src/contexts/axiosRequest.js
+++ src/contexts/axiosRequest.js
@@ -1,5 +1,10 @@
 export const requests = {
+    // Post
     fetchScream: '/scream',
     fetchLogin: '/login',
     fetchSignup: '/signup',
+    fetchUploadImage: '/user/image',
+
+    // Get
+    fetchUser: '/user',
 }
